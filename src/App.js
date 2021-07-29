115e6fbf511b552a2cd48400cfec1989c6cf7e1b
hassan-nasir <hassannasir097@gmail.com>
2021-07-29 23:23:19 +0500

    console added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React,{useEffect} from 'react';
-import './App.css';
-import Chat from './Containers/Chat/Chat';
-import SideBar from './Containers/SideBar/SideBar';
-import {useSelector,useDispatch} from 'react-redux'
-import {selectUser} from './app/features/userSlice'
-import Login from './Containers/Login/Login';
-import { auth } from './Config/Firebase';
-import {login,logout} from './app/features/userSlice'
+import React, { useEffect } from "react";
+import "./App.css";
+import Chat from "./Containers/Chat/Chat";
+import SideBar from "./Containers/SideBar/SideBar";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser } from "./app/features/userSlice";
+import Login from "./Containers/Login/Login";
+import { auth } from "./Config/Firebase";
+import { login, logout } from "./app/features/userSlice";
 
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+  console.log("adasdas");
 
   useEffect(() => {
-    auth.onAuthStateChanged((userState)=>{
+    auth.onAuthStateChanged((userState) => {
       if (userState) {
         dispatch(
           login({
-            uid :userState.uid,
-            photo:userState.photoURL,
-            displayName:userState.displayName,
-            email:userState.email
+            uid: userState.uid,
+            photo: userState.photoURL,
+            displayName: userState.displayName,
+            email: userState.email,
           })
-        )
+        );
       } else {
-        dispatch(logout())
+        dispatch(logout());
       }
-    })
-  }, [dispatch])
+    });
+  }, [dispatch]);
   return (
     <div className="app">
-      {user ? 
-      <>
-        <SideBar/>
-        <Chat/>
-      </> :
-      <Login/>}
-    
+      {user ? (
+        <>
+          <SideBar />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
